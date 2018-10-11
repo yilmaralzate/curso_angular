42ac2081dab51b201f4024ef989fdc34c556ffc1
Yilmar Alzate Rodriguez <yilmar.i.am@gmail.com>
2018-10-11 12:20:52 -0500

    Organizacion del proyecto para el curso

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    //template: `<h1>Titulo</h1>`
+    templateUrl: './header.component.html'
+})
+
+export class HeaderComponent {
+    constructor() {
+        //Nada por ahora
+    }
+}
